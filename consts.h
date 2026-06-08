fd651cc085ee94ff06720a1c886a511e08d07e73
Amit Shalvi Kali <a.shalvikali@gmail.com>
2026-06-08 18:37:18 +0300

    cont

--- consts.h
+++ consts.h
@@ -3,7 +3,7 @@
 
 #define BLOCK_SIZE (64)
 #define NUM_SETS (64)
-#define LINE_SEPERATION_IN_BYTES (NUM_SETS * BLOCK_SIZE)
+#define LINE_SEPARATION_IN_BYTES (NUM_SETS * BLOCK_SIZE)
 
 // Assuming assignment to logical cores 0-11
 #define NUM_LINES (12)
