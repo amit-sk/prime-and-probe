8458c956304bf33201f3a83db381d39d33a4274f
Amit Shalvi Kali <shalvikali@mail.tau.ac.il>
2026-07-08 13:56:43 +0300

    reverting to 12-associativity

--- consts.h
+++ consts.h
@@ -4,10 +4,10 @@
 #define BLOCK_SIZE (64)
 #define NUM_SETS (64)
 #define LINE_SEPARATION_IN_BYTES (NUM_SETS * BLOCK_SIZE)
-#define NUM_LINES (8)  // Assuming assignment to logical cores 0-7
+#define NUM_LINES (12)  // Assuming assignment to logical cores 0-11
 #define CACHE_SIZE (NUM_SETS * NUM_LINES * BLOCK_SIZE)
 
-// assignment allows values from 0-8
+// assignment allows values from 0-12
 #define VICTIM_NUM_LINES_OPTIONS (NUM_LINES + 1)
 
 #endif /* CONSTS_H */
